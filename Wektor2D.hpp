da6169b33fc35b27e46eaad552bf69aba798f2a5
student <student@DESKTOP-Q7G8C5F>
2025-10-22 14:36:49 +0200

    moze teraz

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,11 +1,5 @@
-#include <print>
 #include <stdio.h>
 
-struct Informer
-{
-    Informer() { std::println("informacja o Konstrukcji"); }
-    ~Informer() { std::println("informacja o destrukcji"); }
-};
 
 class Wektor2D
 {
@@ -14,23 +8,19 @@ public:
     {
         x = wspx;
         y = wspy;
-        std::println("Konstruktor niedomyslny: wspx={}, wspy={}", x, y);
     }
-    ~Wektor2D() { std::println("Destrukcja"); }
+    ~Wektor2D() { ; }
     void   setX(double wspx) { x = wspx; }
     void   setY(double wspy) { y = wspy; }
     double getX()
     {
-        std::println("wspx = {}", x);
         return x;
     }
     double getY()
     {
-        std::println("wspy = {}", y);
         return y;
     }
     double   x, y;
-    Informer info;
 };
 
 /*
@@ -53,12 +43,10 @@ struct Iloczyn
 
 Wektor2D operator+(Wektor2D Ve1, Wektor2D Ve2)
 {
-    std::println("Suma wektorow: wspx={}, wspy={}", Ve1.x + Ve2.x, Ve1.y + Ve2.y);
     return Wektor2D{Ve1.x + Ve2.x, Ve1.y + Ve2.y};
 }
 
 double operator*(Wektor2D Ve1, Wektor2D Ve2)
 {
-    std::println("iloczyn wektorowy: {}", Ve1.x * Ve2.x + Ve2.y * Ve1.y);
     return {Ve1.x * Ve2.x + Ve2.y * Ve1.y};
 }
